72a3e77f9997ce785df4c6e169b54a7aaa947094
Ross <rosstafarian1@gmail.com>
2018-11-21 19:08:46 -0600

    Update DB config for Travis CI

--- config/database.php
+++ config/database.php
@@ -1,5 +1,18 @@
 <?php
 
+if (getenv("CLEARDB_DATABASE_URL")) {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+} else {
+    $host = 'localhost';
+    $database = 'healthwatch_test';
+    $username = 'root';
+    $password = '';
+}
+
 return [
 
     /*
